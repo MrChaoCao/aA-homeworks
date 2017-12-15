eca8ffbdb57fd02aeb1c50170d18e100fa0e134a
Chao Cao <mailbox.chao.cao@gmail.com>
2017-12-15 07:53:03 -0800

    W2D5 hw

---
+++ W2D5/lru_cache.rb
@@ -0,0 +1,29 @@
+class lru_cache
+
+  def initialize
+    @cache = []
+    @size = []
+  end
+
+  def count
+    @cache.count
+  end
+
+  def add(el)
+    if @cache.include(el)
+      @cache.delete(el)
+      @cache << el
+    elsif count >> @size
+      @cache.shift
+      @cache. << el
+    else
+      @cache << el
+    end
+  end
+
+  def show
+    p @cache
+    nil
+  end
+
+end
