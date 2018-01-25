62456e998bd59d0033ff309221739f68e8049616
Chao Cao <mailbox.chao.cao@gmail.com>
2018-01-25 02:05:56 -0800

    rainbow routes

---
+++ W7D4/rainbow_routes/components/rainbow.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+  Route,
+  Link,
+  NavLink
+} from 'react-router-dom';
+
+import Red from './red';
+import Green from './green';
+import Blue from './blue';
+import Violet from './violet';
+
+class Rainbow extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Rainbow Router!</h1>
+        {/* Your links should go here */}
+          <NavLink to="/red" ></NavLink>
+          <NavLink to="/green" ></NavLink>
+          <NavLink to="/blue" ></NavLink>
+          <NavLink to="/violet" ></NavLink>
+        <div id="rainbow">
+          {/* Your routes should go here */}
+          <Route path="/red" component={Red} />
+          <Route path="/green" component={Green} />
+          <Route path="/blue" component={Blue} />
+          <Route path="/violet" component={Violet} />
+        </div>
+      </div>
+    );
+  }
+};
+
+export default Rainbow;
